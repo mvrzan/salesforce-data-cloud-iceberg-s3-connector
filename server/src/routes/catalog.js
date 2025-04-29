a4a85cf6f667c8d606603cefe6f347754b7e6bbb
mvrzan <mvrzan@icloud.com>
2025-04-29 18:25:54 -0400

    add route for retrieving tables by namespace and table name

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -3,11 +3,13 @@ import config from "../controllers/config.js";
 import namespaces from "../controllers/namespaces.js";
 import namespaceName from "../controllers/namespaceName.js";
 import middleware from "../middleware/middleware.js";
+import tableName from "../controllers/tableName.js";
 
 const catalogRoutes = Router();
 
 catalogRoutes.get("/v1/config", middleware, config);
 catalogRoutes.get("/v1/namespaces", middleware, namespaces);
 catalogRoutes.get("/v1/namespaces/:namespaceName/tables", middleware, namespaceName);
+catalogRoutes.get("/v1/namespaces/:namespacesName/tables/:tableName", middleware, tableName);
 
 export default catalogRoutes;
