16ed8ef422bb2a4462dc0afde1230bbefd790631
mvrzan <mvrzan@icloud.com>
2025-04-29 16:42:35 -0400

    refactor catalog routes to use namespaceName controller for table retrieval

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import config from "../controllers/config.js";
 import namespaces from "../controllers/namespaces.js";
-import namespaceOne from "../controllers/namespaceOne.js";
+import namespaceName from "../controllers/namespaceName.js";
 import middleware from "../middleware/middleware.js";
 
 const catalogRoutes = Router();
 
 catalogRoutes.get("/v1/config", middleware, config);
 catalogRoutes.get("/v1/namespaces", middleware, namespaces);
-catalogRoutes.get("/v1/namespaces/namespace_onenamespace_one/tables", middleware, namespaceOne);
-catalogRoutes.get("/v1/namespaces/namespace_one/tables", middleware, namespaceOne);
+catalogRoutes.get("/v1/namespaces/:namespaceName/tables", middleware, namespaceName);
 
 export default catalogRoutes;
