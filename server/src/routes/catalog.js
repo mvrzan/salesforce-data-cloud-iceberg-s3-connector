39495bbe316feb81ef09d073865be631ccc7997c
mvrzan <mvrzan@icloud.com>
2025-04-30 11:07:05 -0400

    fix: correct import and route parameter for namespaceTables in catalogRoutes

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import config from "../controllers/config.js";
 import namespaces from "../controllers/namespaces.js";
-import namespaceName from "../controllers/namespaceName.js";
+import namespaceTables from "../controllers/namespaceTables.js";
 import middleware from "../middleware/middleware.js";
 import tableName from "../controllers/tableName.js";
 
@@ -9,7 +9,7 @@ const catalogRoutes = Router();
 
 catalogRoutes.get("/v1/config", middleware, config);
 catalogRoutes.get("/v1/namespaces", middleware, namespaces);
-catalogRoutes.get("/v1/namespaces/:namespaceName/tables", middleware, namespaceName);
+catalogRoutes.get("/v1/namespaces/:namespaceTables/tables", middleware, namespaceTables);
 catalogRoutes.get("/v1/namespaces/:namespacesName/tables/:tableName", middleware, tableName);
 
 export default catalogRoutes;
