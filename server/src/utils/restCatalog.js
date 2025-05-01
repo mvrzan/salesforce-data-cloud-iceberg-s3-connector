8fda3bcde3a128b0c900565bc1020170bc899775
mvrzan <mvrzan@icloud.com>
2025-05-01 10:01:57 -0400

    create restCatalog.js with namespace and object definitions

---
+++ server/src/utils/restCatalog.js
@@ -0,0 +1,15 @@
+export const namespacesInfo = {
+  namespaces: [["Database_namespace_one"], ["Database_namespace_two"]],
+};
+
+export const NamespaceOneObjects = {
+  identifiers: [{ name: "users" }, { name: "user_activities" }],
+};
+
+export const NamespaceTwoObjects = {
+  identifiers: [{ name: "products" }, { name: "inventory" }],
+};
+
+export const FallbackObjects = {
+  identifiers: [{ name: "namespace_not_detected" }, { name: "namespace_not_detected" }],
+};
