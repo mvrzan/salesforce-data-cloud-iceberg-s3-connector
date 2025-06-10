186aea49203ee911bcf41d50dc699aa87cc0a213
mvrzan <mvrzan@icloud.com>
2025-06-09 22:17:43 -0400

    refactor: enhance middleware to include token validation and authorization checks

--- server/src/middleware/middleware.js
+++ server/src/middleware/middleware.js
@@ -1,7 +1,25 @@
 import { getCurrentTimestamp } from "../utils/loggingUtil.js";
 
-const middleware = (req, _res, next) => {
+const middleware = (req, res, next) => {
   console.log(`${getCurrentTimestamp()} - 🚨 middleware - Request: ${req.method} ${req.originalUrl}`);
+
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token === null) {
+    console.log(`${getCurrentTimestamp()} - 🚨 middleware - Unauthorized: No token provided!`);
+    return res.status(401).send({
+      error: "Unauthorized: No token provided",
+    });
+  }
+
+  if (token !== process.env.CLIENT_ACCESS_TOKEN) {
+    console.log(`${getCurrentTimestamp()} - 🚨 middleware - Forbidden: Invalid token!`);
+
+    return res.status(403).send({
+      error: "Forbidden: Invalid token!",
+    });
+  }
 
   if (req.url.includes("%1F")) {
     const cleanedUrl = req.url.replace(/%1F/g, "");
