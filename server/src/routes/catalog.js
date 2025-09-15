caabb86e5f5b6979144f2743a3a1f976612b6020
mvrzan <mvrzan@icloud.com>
2025-09-15 12:58:27 -0400

    add namespace details endpoint

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import config from "../controllers/config.js";
 import namespaces from "../controllers/namespaces.js";
+import namespaceDetails from "../controllers/namespaceDetails.js";
 import namespaceTables from "../controllers/namespaceTables.js";
 import middleware from "../middleware/middleware.js";
 import tableName from "../controllers/tableName.js";
@@ -9,6 +10,7 @@ const catalogRoutes = Router();
 
 catalogRoutes.get("/v1/config", middleware, config);
 catalogRoutes.get("/v1/namespaces", middleware, namespaces);
+catalogRoutes.get("/v1/namespaces/:namespace", middleware, namespaceDetails);
 catalogRoutes.get("/v1/namespaces/:namespace/tables", middleware, namespaceTables);
 catalogRoutes.get("/v1/namespaces/:namespacesName/tables/:tableName", middleware, tableName);
 
