439dd568921d75d1ec472f9ec79fda2b8c7bb0f9
mvrzan <mvrzan@icloud.com>
2025-10-09 09:54:16 -0400

    add body-parser middleware for JSON and URL-encoded request handling

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import bodyParser from "body-parser";
 import catalogRoutes from "./src/routes/catalog.js";
 import { getCurrentTimestamp } from "./src/utils/loggingUtil.js";
 
 const app = express();
 const port = process.env.APP_PORT || process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(catalogRoutes);
 
 app.listen(port, () => {
